403e15477537ed893a45465eee4150428f7d55eb
sultyl <sultyleke@gmail.com>
2023-04-02 03:19:46 +0100

    build folder

---
+++ build/precache-manifest.0848867c3c38802a5b59e1157fa8c6db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7e6a0dc79a8456995ff",
+    "url": "/DevSultan/static/css/main.b9daa5e4.chunk.css"
+  },
+  {
+    "revision": "f7e6a0dc79a8456995ff",
+    "url": "/DevSultan/static/js/main.f7e6a0dc.chunk.js"
+  },
+  {
+    "revision": "7e1f1647bb30cfcd7bd8",
+    "url": "/DevSultan/static/js/1.7e1f1647.chunk.js"
+  },
+  {
+    "revision": "0150e6ea115801ca381c",
+    "url": "/DevSultan/static/js/runtime~main.0150e6ea.js"
+  },
+  {
+    "revision": "48ef1e8cf26d1e71e9f66127584f042c",
+    "url": "/DevSultan/static/media/Ola.48ef1e8c.png"
+  },
+  {
+    "revision": "03f8a775fcad9be0062a1e52a29b00f4",
+    "url": "/DevSultan/static/media/Bus.03f8a775.jpg"
+  },
+  {
+    "revision": "2d8356d7fcb578850b9ef0fd69d2b175",
+    "url": "/DevSultan/static/media/fit.2d8356d7.jpg"
+  },
+  {
+    "revision": "698c63ccdb9e5768a5d5b99824c73551",
+    "url": "/DevSultan/static/media/Por.698c63cc.jpg"
+  },
+  {
+    "revision": "6fe3d6997d02b5ab35bb5444abd09908",
+    "url": "/DevSultan/static/media/Res.6fe3d699.jpg"
+  },
+  {
+    "revision": "02d1539d1633609e38794a6414b5b3a0",
+    "url": "/DevSultan/static/media/Job.02d1539d.jpg"
+  },
+  {
+    "revision": "b7cfe94f6500863e774de89ecddf8d24",
+    "url": "/DevSultan/static/media/odogwu.b7cfe94f.png"
+  },
+  {
+    "revision": "b451109726855e8d335db3fadd44f23e",
+    "url": "/DevSultan/index.html"
+  }
+];
